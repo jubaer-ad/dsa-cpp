46ed32c87adfe00bf28c560f658c0ba02a649417
jubaer.ad <jubaerad1@gmail.com>
2023-08-19 23:18:12 +0600

    initialisedArray updated

--- InitialisedArray.cpp
+++ InitialisedArray.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int arr[], int size)
+{
+    cout << "Array is as below: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
 int main()
 {
-    int size;
+    int size = 8;
     cout << "Enter array size: " << endl;
     cin >> size;
-    int defaultValue;
+    int defaultValue = 1;
     cout << "Enter default value: " << endl;
     cin >> defaultValue;
+
     int newArray[size];
 
     for (int i = 0; i < size; i++)
@@ -16,11 +26,7 @@ int main()
         newArray[i] = defaultValue;
     }
 
-    cout << "Array is as below: " << endl;
-    for (int i = 0; i < size; i++)
-    {
-        cout << newArray[i] << " ";
-    }
+    printArray(newArray, size);
     cout << endl;
     return 0;
 }
