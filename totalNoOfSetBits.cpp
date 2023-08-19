1e23f0a31446147110f9b534cd6cdf73794e6d17
jubaer.ad <jubaerad1@gmail.com>
2023-08-19 17:50:02 +0600

    setBits

--- totalNoOfSetBits.cpp
+++ totalNoOfSetBits.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
+int totalSetBits(int num)
+{
+    int noSetBits = 0;
+    if (num == 0)
+    {
+        return noSetBits;
+    }
+    while (num != 0)
+    {
+        noSetBits += num & 1;
+        num = num >> 1;
+    }
+    return noSetBits;
+}
 int main()
 {
-    cout << "Hello";
-    return 0;
+    cout << "Hello" << endl;
+    int a, b;
+    cin >> a >> b;
+    cout << "setBits: " << totalSetBits(a) + totalSetBits(b) << endl;
 }
