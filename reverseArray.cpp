43e12170e3d72d6622006be1ef14764ce698fc07
jubaer.ad <jubaerad1@gmail.com>
2023-08-22 23:46:53 +0600

    reverseArray updated

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
-void printArray(int arr[], int size)
+void printArray(int arr[], int size, bool isOriginal)
 {
+    if (isOriginal)
+        cout << "Original";
+    else
+        cout << "Reversed";
+    cout << "-";
+    Sleep(300);
+    cout << "-";
+    Sleep(300);
+    cout << "-";
+    Sleep(300);
+    cout << ">";
+    Sleep(300);
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
+        if (isOriginal)
+            Sleep(50);
+        else
+            Sleep(100);
+        if (i == size / 2 && !isOriginal)
+            Sleep(500);
     }
     cout << endl;
+}
+
+void swap(int arr[], int pos1, int pos2)
+{
+    arr[pos1] = arr[pos1] + arr[pos2];
+    arr[pos2] = arr[pos1] - arr[pos2];
+    arr[pos1] = arr[pos1] - arr[pos2];
 }
 
 void reverseArray(int arr[], int size)
 {
     for (int i = 0; i < size / 2; i++)
     {
-        arr[i] = arr[i] + arr[size - 1 - i];
-        arr[size - 1 - i] = arr[i] - arr[size - 1 - i];
-        arr[i] = arr[i] - arr[size - 1 - i];
+        swap(arr, i, size - 1 - i);
     }
 }
+
 int main()
 {
     int size, num;
@@ -34,7 +59,8 @@ int main()
             cout << i << ": ";
         cin >> arr[i];
     }
+    printArray(arr, size, 1);
     reverseArray(arr, size);
-    printArray(arr, size);
+    printArray(arr, size, 0);
     return 0;
 }
